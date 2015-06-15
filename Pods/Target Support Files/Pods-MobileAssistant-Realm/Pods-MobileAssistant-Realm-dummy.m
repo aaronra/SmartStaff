13e5717131ac8158cb24372aee0ffbbd34dcf695
t0tep <neon0619@gmail.com>
2015-06-15 10:59:33 +0800

    deleted

--- Pods/Target Support Files/Pods-MobileAssistant-Realm/Pods-MobileAssistant-Realm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAssistant_Realm : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAssistant_Realm
-@end
--- Pods/Target Support Files/Pods-MobileAssistant-RealmSwift/Pods-MobileAssistant-RealmSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAssistant_RealmSwift : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAssistant_RealmSwift
-@end
